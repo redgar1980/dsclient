80b3fc7aed1f6dc60cf3c4e077eb39b4409e66e8
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-02-04 18:53:47 -0300

    primeiro commit

---
+++ src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -0,0 +1,78 @@
+package com.devsuperior.dsclient.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsclient.dto.ClientDTO;
+import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.repositories.ClientRepository;
+import com.devsuperior.dsclient.services.exceptions.DatabaseException;
+import com.devsuperior.dsclient.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ClientService {
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);		
+		return list.map(client -> new ClientDTO(client));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Não foi possível encontrar o cliente com id " + id));
+		
+		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getReferenceById(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Cliente não foi encontrado o id " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Cliente não foi encontrado o id " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de integridade");
+		}		
+	}
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('César Joaquim Aparício', '63093988659', 263.51, '2005-01-23', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Priscila Joana Ayla Costa', '36907975866', 350.23, '1956-01-18', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Daniela Raimunda Malu Mendes', '98578374339', 423.74, '1991-02-04', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Raimundo Nelson Matheus Viana', '73851984498', 143.16, '1996-02-03', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sophie Eduarda da Costa', '59827447947', 463.86, '1987-02-03', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bianca Luciana Isis Nunes', '87246319735', 531.51, '1951-02-04', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ayla Olivia Rita Brito', '62143441568', 763.71, '1985-01-01', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Filipe Pedro Henrique da Costa', '63093988659', 263.51, '1997-05-23', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Amanda Stefany Andrea de Paula', '79144697694', 143.95, '1998-01-16', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Anthony Levi Francisco Peixoto', '86412813802', 564.34, '1961-01-08', 2);
